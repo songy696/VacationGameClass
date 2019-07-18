9b60e15299b1e3307973f71ef5153049492aede3
songy696 <ysong.yjs@gmail.com>
2019-07-18 15:50:02 +0900

    enemy

---
+++ air and stone/Assets/Scripts/enemyScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour
+{
+
+    public GameObject[] TrailPoints;
+    public Transform enemy;
+
+    void Awake()
+    {
+        StartCoroutine(TrailEnemies());
+    }
+
+    IEnumerator TrailEnemies() {
+
+        while (true) {
+            yield return new WaitForSeconds(Random.Range(0.5f, 1f));
+            TrailEnemy();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TrailEnemy()
+    {
+        TrailPoints = GameObject.FindGameObjectsWithTag("TrailPoints");
+
+        GameObject randomEnemyTrails = TrailPoints[Random.Range(0, TrailPoints.Length)];
+
+        Vector3 TrailPath = transform.position;
+
+        TrailPath = new Vector3(10, 0, 0);
+    }
+
+}
