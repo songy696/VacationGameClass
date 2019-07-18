f4a1961a97c4cdb35f864d28f9b6b1961e3a1ed4
songy696 <ysong.yjs@gmail.com>
2019-07-18 15:49:44 +0900

    enemy movement

---
+++ air and stone/Assets/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public enemyScript enemyTrails;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
